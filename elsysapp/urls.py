9b983e365b33cbed1d31c3c4436982959b01db8a
Ole-Jakob SHT <ole.schubert@shifthyperloop.com>
2022-11-03 15:10:03 +0100

    gjort at man kan local runa nettsiden

--- elsysapp/urls.py
+++ elsysapp/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from .views import index #Relativ import av viewsfunksjonen
 
-appname = "elsysapp"
+appname = "nettside_hlrdukke"
 urlpatterns = [
     path('', index, name='index'),
 ]
 
+    
